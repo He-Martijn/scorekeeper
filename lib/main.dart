6d7a19db045d2d0be7a29a2fba4af1245fa1b470
He-Martijn <roert.stapje_0o@icloud.com>
2022-01-21 16:47:02 +0100

    Initial commit: Start of the project

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Is the following true?',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: MyBodyApp(),
+      ),
+    );
+  }
+}
+
+class MyBodyApp extends StatefulWidget {
+  const MyBodyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyBodyAppState createState() => _MyBodyAppState();
+}
+
+class _MyBodyAppState extends State<MyBodyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.grey.shade900,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 5,
+              child: Center(
+                child: Text('Good question',
+                style: TextStyle(
+                  color: Colors.white,
+                  ),
+                ),
+              ),
+          ),
+          Expanded(
+            flex: 1,
+              child: TextButton(
+                onPressed: (){
+                  print('true is pressed');
+                },
+                child: Container(
+                  width: 200.0,
+                  height: 75.0,
+                  color: Colors.green,
+                ),
+              ),
+          ),
+          Expanded(
+            flex: 1,
+            child: TextButton(
+              onPressed: (){
+                print('false is pressed');
+              },
+              child: Container(
+                width: 200.0,
+                height: 75.0,
+                color: Colors.red,
+              ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
