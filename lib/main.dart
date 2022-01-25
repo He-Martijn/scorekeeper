59a68951c626041765cac098235c9356f83b1cd1
He-Martijn <roert.stapje_0o@icloud.com>
2022-01-25 20:49:23 +0100

    Finish the app my own way

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,21 @@ class _MyBodyAppState extends State<MyBodyApp> {
 
   //Variables
   List <Icon> scoreKeeper = [];
+
+  List <List> questionAndAnswers = [
+    ['You can lead a cow down stairs,but not upstairs.',
+      'Approx. one quarter of the human bones are in the feet',
+      'A slug\'s blood is green',
+      'A \ and \' is a String in Android Studio'
+
+    ],[
+      false,
+      true,
+      true,
+      true
+    ]
+  ];
+
   List <String> questions = [
     'You can lead a cow down stairs,but not upstairs.',
     'Approx. one quarter of the human bones are in the feet',
@@ -50,16 +65,58 @@ class _MyBodyAppState extends State<MyBodyApp> {
   ];
 
   //Functions
-  String pickQuestion(){
+  int nextQuestion = 0;
+
+  int pickQuestion(){
     print('pickQuestion is called');
     int maxInt = questions.length;
     print('maxInt = $maxInt');
     int randomInt = Random().nextInt(maxInt);
     print('randomInt = $randomInt');
-    String pickedSentence = questions[randomInt];
-    print('pickedSentence = $pickedSentence');
 
-    return pickedSentence;
+    return randomInt;
+  }
+
+  String question(int questionNumber){
+    List questionList = questionAndAnswers[0];
+    String thisQuestion = questionList[questionNumber];
+    print('thisQuestion = $thisQuestion');
+
+    return thisQuestion;
+
+  }
+
+  bool rightAnswer(int questionNumber){
+    List answerList = questionAndAnswers[1];
+    bool thisAnswer = answerList[questionNumber];
+    print('thisAnswer = $thisAnswer');
+
+    return true;
+  }
+
+  void checkAnswer(int answerNumber, bool givenAnswer){
+    List answerList = questionAndAnswers[1];
+    bool rightAnswerHere = answerList[answerNumber];
+
+    if (givenAnswer==rightAnswerHere){
+      print('answer is right');
+      setState(() {
+        scoreKeeper.add(Icon(Icons.check, color: Colors.green,),);
+      });
+    } else {
+      print('answer is wrong');
+        scoreKeeper.add(Icon(Icons.close, color: Colors.red,),);
+    };
+
+  }
+
+  String firstQuestion(){
+    print('firstQuestion is called');
+    nextQuestion = pickQuestion();
+    List questionList = questionAndAnswers[0];
+    String question = questionList[nextQuestion];
+
+    return question;
   }
 
   @override
@@ -74,7 +131,7 @@ class _MyBodyAppState extends State<MyBodyApp> {
           Expanded(
             flex: 5,
               child: Center(
-                child: Text(pickQuestion(),
+                child: Text(firstQuestion(),
                 style: TextStyle(
                   color: Colors.white,
                   ),
@@ -87,8 +144,10 @@ class _MyBodyAppState extends State<MyBodyApp> {
                 onPressed: (){
                   print('true is pressed');
                   setState(() {
-                    scoreKeeper.add(Icon(Icons.check, color: Colors.green,),);
-                    pickQuestion();
+                    //scoreKeeper.add(Icon(Icons.check, color: Colors.green,),);
+                    checkAnswer(nextQuestion,true);
+                    nextQuestion = pickQuestion();
+                    question(nextQuestion);
                   });
                 },
                 child: Container(
@@ -110,8 +169,11 @@ class _MyBodyAppState extends State<MyBodyApp> {
               onPressed: (){
                 print('false is pressed');
                 setState(() {
-                  scoreKeeper.add(Icon(Icons.close,color: Colors.red,),);
-                  pickQuestion();
+                  //scoreKeeper.add(Icon(Icons.close,color: Colors.red,),);
+                  //pickQuestion();
+                  checkAnswer(nextQuestion,false);
+                  nextQuestion = pickQuestion();
+                  question(nextQuestion);
                 });
               },
               child: Container(
